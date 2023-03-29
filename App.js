2617b85bc560e9a2b0471cc3428cec571f975c9c
Sanju2605 <108916675+Sanju2605@users.noreply.github.com>
2023-03-29 10:05:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Car,Location} from './Location'
+
+function App() {
+  return (
+    <div className="App">
+      <Car/>
+      <Location cname="Jaguar" cyname="Britain"></Location>
+    </div>
+  );
+}
+
+export default App;
